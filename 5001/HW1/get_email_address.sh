cbb9d4d554a86feff2148663fff25bacfeb987c1
RubyLau <31153683+RubyLau@users.noreply.github.com>
2020-10-19 23:28:24 +0800

    Update get_email_address.sh

--- 5001/HW1/get_email_address.sh
+++ 5001/HW1/get_email_address.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-grep "id=\"" blocklist.xml | egrep "id=\"([a-zA-Z0-9._-]+)@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+\"" blocklist.xml | awk '{print}' $1 > id_filter_results.txt
+grep "id=\"" blocklist.xml | egrep "id=\"([a-zA-Z0-9._-]+)@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+\"" blocklist.xml > id_filter_results.txt
 cat id_filter_results.txt
